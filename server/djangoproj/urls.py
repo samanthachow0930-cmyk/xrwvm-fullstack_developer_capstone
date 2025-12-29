42de8b922d0f6537bd788a167a046f1efec849e9
samanthachow0930-cmyk <samanthachow0930@gmail.com>
2025-12-29 03:19:17 -0500

    fix deployment

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 from django.views.generic import TemplateView
 from django.conf.urls.static import static
 from django.conf import settings
+from django.urls import re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -35,3 +36,7 @@ urlpatterns = [
         'postreview/<int:dealer_id>',
         TemplateView.as_view(template_name="index.html")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns += [
+    re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
+]
